a45e89af9c04b610c2bd210a70f03fb6fab2b38f
David Greaves <david@dgreaves.com>
2010-06-16 22:35:44 +0100

    Simple demo CITA automated test system

--- demo/cita/lib/cita.rb
+++ demo/cita/lib/cita.rb
@@ -1,4 +1,30 @@
-# Your starting point for daemon specific classes. This directory is
-# already included in your load path, so no need to specify it.
+class CITA < DaemonKit::RuotePseudoParticipant
 
-require 'sample'
+  on_exception :dammit
+
+  on_complete do |workitem|
+    workitem['success'] = true
+  end
+
+  def test
+    2.times { puts "" }
+    puts "Testing an image with  #{workitem['pkg']} in it"
+    if rand(10) > 5 then
+      workitem.fields["test_result"]="OK"
+    else
+      workitem.fields["test_result"]="BAD"
+    end
+    puts "It was #{workitem.fields['test_result']}"
+    2.times { puts "" }
+  end
+
+  def err
+    print "Error"
+    raise ArgumentError, "Does not compute"
+  end
+
+  def dammit( exception )
+    workitem["error"] = exception.message
+  end
+
+end
--- demo/cita/libexec/cita-daemon.rb
+++ demo/cita/libexec/cita-daemon.rb
@@ -25,7 +25,7 @@ DaemonKit::RuoteParticipants.configure do |config|
 
   # Register your classes as pseudo-participants, with work being delegated
   # according to the 'command' parameter passed in the process definition
-  config.register Sample
+  config.register CITA
 end
 
 DaemonKit::RuoteParticipants.run do
