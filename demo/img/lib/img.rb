9a44d79fa86ad66bf5bede7aa6f76fbc1915a7af
David Greaves <david@dgreaves.com>
2010-06-16 22:33:59 +0100

    Simple demo IMG image creator

--- demo/img/lib/img.rb
+++ demo/img/lib/img.rb
@@ -1,4 +1,29 @@
-# Your starting point for daemon specific classes. This directory is
-# already included in your load path, so no need to specify it.
+class IMG < DaemonKit::RuotePseudoParticipant
 
-require 'sample'
+  on_exception :dammit
+
+  on_complete do |workitem|
+    workitem['success'] = true
+  end
+
+  def build
+    2.times { puts "" }
+    image_time = rand 10
+    puts "You need an image with  #{workitem['pkg']}.... just wait #{image_time} seconds"
+    sleep image_time
+    workitem.fields["image-time"] = image_time
+    workitem.fields["build_ok"] = (rand(2)>0)?"YES":"NO"
+    puts "Ready. Image built successfully: #{workitem.fields["build_ok"]}"
+    2.times { puts "" }
+  end
+
+  def err
+    print "Error"
+    raise ArgumentError, "Does not compute"
+  end
+
+  def dammit( exception )
+    workitem["error"] = exception.message
+  end
+
+end
