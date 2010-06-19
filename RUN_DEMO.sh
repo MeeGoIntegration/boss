ada4c0fe693a1bc95a9106ceaaa9cb7548cfc22b
David Greaves <david@dgreaves.com>
2010-06-19 10:07:08 +0100

    Ensure xterms don't die and leave rogue daemon-kit participants eating jobs and causing confusion!

--- RUN_DEMO.sh
+++ RUN_DEMO.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 # Start MQ system
-#ssh amqpvm
-tail -f /var/log/rabbitmq/rabbit.log
+# ssh amqpvm tail -f /var/log/rabbitmq/rabbit.log &
 
 # Start (a) workflow engine
-xterm -T ENGINE -e demo/ENGINE.sh &
+nohup xterm -T ENGINE -e demo/ENGINE.sh 2>/dev/null &
 # Start a proxy for OBS
-xterm -T BUILDER -e demo/OBS.sh &
+nohup xterm -T BUILDER -e demo/OBS.sh 2>/dev/null &
 # Start a proxy for CITA
-xterm -T TESTER -e demo/CITA.sh &
+nohup xterm -T TESTER -e demo/CITA.sh 2>/dev/null &
 # Start a proxy for IMG
-xterm -T IMAGER -e demo/IMG.sh
+nohup xterm -T IMAGER -e demo/IMG.sh 2>/dev/null &
 
 # 
 echo run : demo/START.sh
