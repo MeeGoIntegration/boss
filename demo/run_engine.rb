825cb455a9bf4094fedb7bd26e282a65900a8ef9
David Greaves <david@dgreaves.com>
2010-06-20 23:32:01 +0100

    Define per-participant classes to provide cleaner process definitions

--- demo/run_engine.rb
+++ demo/run_engine.rb
@@ -13,19 +13,47 @@ require 'ruote'
 require 'ruote/storage/fs_storage'
 require 'ruote-amqp'
 
+# Particularly useful for JSON.pretty_generate workitem.to_h
+require 'yajl/json_gem'
+
 AMQP.settings[:host] = 'amqpvm'
 AMQP.settings[:user] = 'ruote'
 AMQP.settings[:pass] = 'ruote'
 AMQP.settings[:vhost] = 'ruote-test'
 #AMQP.logging = true
 
+engine = Ruote::Engine.new(
+  Ruote::Worker.new(
+    Ruote::FsStorage.new('/tmp/work')
+  )
+)
+
+# This spawns a thread which listens for amqp responses
+RuoteAMQP::Receiver.new( engine )
+
+# This registers a general purpose 'remote' participant
+engine.register_participant( 'remote', RuoteAMQP::Participant )
+
+# A logging error handler
+class ErrorHandler
+  include Ruote::LocalParticipant
+  def consume (workitem)
+    puts "Error in workitem:"
+    puts JSON.pretty_generate workitem.to_h
+  end
+end
+engine.register_participant( 'shout', ErrorHandler )
+
+# A local participant  
 class DeveloperParticipant
   include Ruote::LocalParticipant
   def initialize (opts)
     @opts = opts
+    puts "opts %s" % opts
   end
   def consume (workitem)
     workitem.fields['pkg'] = "shopper #{rand 5}.#{rand 10}.#{rand 10}"
+    workitem.fields['pkg'] = "shopper 1.2.3"
     puts "I've developed a package: #{workitem.fields['pkg']}"
     reply_to_engine(workitem)
   end
@@ -34,14 +62,45 @@ class DeveloperParticipant
     # never 'holding' a workitem
   end
 end
+engine.register_participant 'developer', DeveloperParticipant
 
-engine = Ruote::Engine.new(
-  Ruote::Worker.new(
-    Ruote::FsStorage.new('/tmp/work')
-  )
-)
-RuoteAMQP::Receiver.new( engine ) # This spawns a thread which listens for amqp responses
+class ParticipantWrapper < RuoteAMQP::Participant
+  def consume( workitem )
+    workitem.params['reply_queue'] = "ruote_workitems" # Set a default
+    workitem.fields["params"] = 
+      workitem.params.merge(route).inject({}) { |h, (k, v)| h[k.to_s] = v; h }
+    super workitem
+  end
+end
 
+class OBS < ParticipantWrapper
+  def route
+    { 'command' => '/obs/build',
+      'queue' => 'obs',
+    }
+  end
+end
+engine.register_participant( 'builder', OBS )
+
+class IMG < ParticipantWrapper
+  def route
+    { 'command' => '/img/image',
+      'queue' => 'img',
+    }
+  end
+end
+engine.register_participant( 'imager', IMG )
+
+class CITA < ParticipantWrapper
+  def route
+    { 'command' => '/cita/test',
+      'queue' => 'cita',
+    }
+  end
+end
+engine.register_participant( 'tester', CITA )
+
+# A block participant
 engine.register_participant( 'print_results' ) do |workitem|
   puts "============="
   puts "Package #{workitem.fields['pkg']}"
@@ -50,17 +109,6 @@ engine.register_participant( 'print_results' ) do |workitem|
   puts "============="
 end
 
-engine.register_participant 'developer', DeveloperParticipant
-
 puts "Engine running"
 
 sleep 50000
-#3.times { puts "" }
-#puts "Launching ErrorProcess"
-#fei = engine.launch( error_process )
-
-#engine.wait_for( fei )
-
-#3.times { puts "" }
-#puts "Errors in engine"
-#puts engine.errors.inspect
--- demo/start_a_process.rb
+++ demo/start_a_process.rb
@@ -3,10 +3,7 @@
 #
 # Requirements:
 # * ruote-2.1.10 or later
-# * ruote-amqp-2.1.10 or later
-# * daemon-kit-0.1.8rc3 or later
 #
-$:.push "./ruote-amqp/lib"
 $:.push "./ruote/lib"
 
 require 'rubygems'
@@ -14,59 +11,33 @@ require 'bundler'
 Bundler.setup
 require 'ruote'
 require 'ruote/storage/fs_storage'
-require 'ruote-amqp'
 
-AMQP.settings[:host] = 'amqpvm'
-AMQP.settings[:user] = 'ruote'
-AMQP.settings[:pass] = 'ruote'
-AMQP.settings[:vhost] = 'ruote-test'
-#AMQP.logging = true
-
+# Describe the engine/storage we're using
 engine = Ruote::Engine.new(
     Ruote::FsStorage.new('/tmp/work')
 )
 
-ci_process = Ruote.process_definition :name => 'Ci Process' do
-  sequence do
-    developer
-    kit :command => '/obs/build', :queue => 'obs', :reply_queue => "ruote_workitems"
-    kit :command => '/img/image', :queue => 'img', :reply_queue => "ruote_workitems"
-    _if '${f:build_ok} == YES' do
-      kit :command => '/cita/test', :queue => 'cita', :reply_queue => "ruote_workitems"
-    end
-    print_results
-  end
-end
+# Note that all participants are pre-registered in the engine
 
-real_ci_process = Ruote.process_definition :name => 'Ci Process' do
-  sequence do
+ci_process = Ruote.process_definition :name => 'Ci Process' do
+  sequence :on_error => 'handle_issue' do
     developer
     builder
     imager
     _if '${f:build_ok} == YES' do
-      sequence do
-        tester
-        announce announcement=>"OK"
-      end
-      announce announcement=>"NOT OK"
+      tester
     end
     print_results
   end
-end
 
-engine.register_participant( 'kit', RuoteAMQP::Participant )
+  define 'handle_issue' do
+    shout :msg => 'process ${wfid} has died'
+  end
+
+end
 
 puts "Launching Process"
 fei = engine.launch( ci_process )
 
 puts "Set it off, exiting"
 
-#3.times { puts "" }
-#puts "Launching ErrorProcess"
-#fei = engine.launch( error_process )
-
-#engine.wait_for( fei )
-
-#3.times { puts "" }
-#puts "Errors in engine"
-#puts engine.errors.inspect
