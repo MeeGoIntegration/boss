428b3f9d476d75640042c677a02b3c0ae74bbddc
David Greaves <david@dgreaves.com>
2011-03-21 17:40:39 +0000

    Rewrite boss-daemon.rb to work under daemontools

--- root/usr/lib/boss/libexec/boss-daemon.rb
+++ root/usr/lib/boss/libexec/boss-daemon.rb
@@ -1,3 +1,7 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+
 require 'ruote'
 require 'ruote/storage/fs_storage'
 require 'ruote-amqp'
@@ -7,46 +11,58 @@ require 'yajl/json_gem'
 
 require 'mq'
 
+# FIXME: use: http://rubygems.org/gems/ini
+#          or http://rubygems.org/gems/inifile
+# and boss.config
+
 #AMQP.logging = true
 AMQP.settings[:host] = 'localhost'
 AMQP.settings[:user] = 'boss'
 AMQP.settings[:pass] = 'boss'
 AMQP.settings[:vhost] = 'boss'
 
-# Do your post daemonization configuration here
-# At minimum you need just the first line (without the block), or a lot
-# of strange things might start happening...
-DaemonKit::Application.running! do |config|
-  # Trap signals with blocks or procs
-  # config.trap( 'INT' ) do
-  #   # do something clever
-  # end
-  config.trap( 'TERM', Proc.new { 
-                 puts 'Going down'
-                 AMQP.stop!
-               } )
-end
-
 $engine = Ruote::Engine.new(
   Ruote::Worker.new(
+    # Use boss.config
     Ruote::FsStorage.new('/var/spool/boss/boss_ruote_db')
   )
 )
 
+# if options.debug
+# $engine.noisy = true
+
+# We run under daemontools and it communicates via signals
+Signal.trap('SIGTERM') do 
+  puts 'Shutdown gracefully'
+  $engine.shutdown
+  puts 'Asked engine to stop'
+end
+
 # This spawns a thread which listens for amqp responses
 RuoteAMQP::Receiver.new( $engine, :launchitems => true )
 
-# A logging error handler
-class ErrorHandler
+# A simple LocalParticipant to handle registering a new AMQP Participant
+class BOSSRegistrar
   include Ruote::LocalParticipant
-  def consume (workitem)
-    puts "Error in workitem:"
-    puts JSON.pretty_generate workitem.to_h
+  def consume(workitem)
+    puts "Register a new participant", workitem.fields["name"]
+    $engine.register_participant(workitem.fields["name"],
+                                 RuoteAMQP::ParticipantProxy,
+                                 :queue => workitem.fields["queue"],
+                                 :position => -2 )
+    reply_to_engine(workitem)
   end
 end
 
-$engine.register_participant( 'error', ErrorHandler )
+$engine.register_participant 'boss_register', BOSSRegistrar
 
+
+# All setup... wait for a shutdown
 puts "Engine running"
 
 $engine.join()
+puts "Engine stopped"
+
+# FIXME: RuoteAMQP is not shutting down nicely
+# RuoteAMQP.stop!
+# puts "AMQP stopped"
