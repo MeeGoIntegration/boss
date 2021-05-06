61e9021b7bf2d6d04c377523fd1c60bbed9d52a4
David Greaves <david.greaves@jolla.com>
2021-05-06 09:57:47 +0100

    Clarify the purpose of config.rb and remove superfluous AMQP code
    Signed-off-by: David Greaves <david@dgreaves.com>

--- lib/boss/config.rb
+++ lib/boss/config.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 require 'ruote'
 require 'pp'
 require 'inifile'
@@ -8,6 +7,10 @@ STDOUT.sync = true
 STDERR.sync = true
 
 require 'optparse'
+
+# config.rb provides a BOSS.storage based on the config file used by
+# boss.  This simplifies writing process handling tools such as
+# boss_clean_errors etc
 
 $stderr.puts "Read configuration\n"
 module BOSS
@@ -27,56 +30,10 @@ module BOSS
 
   $stderr.puts "Read configuration as:\n#{conf}\n" if debug
 
-  amqp_host, amqp_port = ( conf["boss"]["amqp_host"] || "127.0.0.1:5672" ).split(":")
-  user = conf["boss"]["amqp_user"] || "boss"
-  pass = conf["boss"]["amqp_pwd"]  || "boss"
-  vhost = conf["boss"]["amqp_vhost"] || "boss"
   db_path = conf["boss"]["db_path"] || "/var/spool/boss/boss_ruote_db"
-  port = Integer(conf["boss"]["viewer_port"]) || 9292
-  bind = conf["boss"]["viewer_address"] || "127.0.0.1"
-  hprio = /!high/
-
-  # Supervisor runs a number of processes each with an identifier
-  # We use that to decide what role we have
-  # Currently
-  # 0 => viewer
-  # 1 => scheduler
-  # 2 => worker
-  pname = ENV["SUPERVISOR_PROCESS_NAME"] || "boss_0"
-  # Yes, this is just dropped into a global variable...
-  $pnum = pname.split("_")[1].to_i
-
-  def set_and_check_amqp
-    AMQP.settings[:host] = amqp_host
-    AMQP.settings[:port] = Integer(amqp_port)
-    AMQP.settings[:user] = user
-    AMQP.settings[:pass] = pass
-    AMQP.settings[:vhost] = vhost
-    AMQP.logging = debug
-
-    #test connection
-    begin
-      AMQP.start() do |connection|
-        AMQP::Channel.new(connection) do |channel|
-          $stderr.puts "Connection opened ok"
-          AMQP.stop { EventMachine.stop }
-        end
-      end
-    rescue AMQP::Error => e
-      $stderr.puts "Failed to connect to AMQP server, error was :"
-      $stderr.puts e.message
-      $stderr.puts "Please check that the settings in /etc/skynet/skynet.conf are correct."
-      $stderr.puts "Run the following commands as root on the AMQP server :"
-      $stderr.puts "/usr/sbin/rabbitmqctl add_vhost #{vhost}"
-      $stderr.puts "/usr/sbin/rabbitmqctl add_user #{user} #{pass}"
-      $stderr.puts "/usr/sbin/rabbitmqctl set_permissions -p #{vhost} #{user} '.*' '.*' '.*'"
-      exit 1
-    end
-  end
 
   $pp_verbose = true;
 
-  # storage = Ruote::BOSSStorage.new(db_path, { :number => $pnum })
   @storage = BOSS::Storage.new(db_path)
   def storage
     @storage
